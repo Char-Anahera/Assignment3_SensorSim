63f6e943fd88d6690c13a72d79e2124787ebc40a
charlize <charl@CHARPUTER>
2025-11-24 01:11:00 +1300

    Initial app set up, sensor object created

---
+++ SensorSim/Models/Sensor.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SensorSim.Models
+{
+    public class Sensor
+    {
+        public string Name {  get; set; }
+        public string Location { get; set; }
+        public double MinTemp { get; set; }
+        public double MaxTemp { get; set; }
+
+
+        public Sensor(string name, string location, double minTemp, double maxTemp  )
+        {
+            Name = name;
+            Location = location;
+            MinTemp = minTemp;
+            MaxTemp = maxTemp;
+        }
+    }
+}
--- SensorSim/Program.cs
+++ SensorSim/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace Sensors
+{
+     internal static class Program
+    {
+        private static void Main(string[] args)
+        {
+
+        }
+    }
+}

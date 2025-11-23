8a9803939f891778370f66673772678f492c2a48
charlize <charl@CHARPUTER>
2025-11-24 01:26:21 +1300

    Confg file created, first sensor added

---
+++ SensorSim/Models/Data.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SensorSim.Models
+{
+    public class Data
+    {
+        public string SensorName { get; set; }
+        public double Temperature {  get; set; }
+        public DateTime DateTime { get; set; }
+
+    }
+}
--- SensorSim/Models/Sensor.cs
+++ SensorSim/Models/Sensor.cs
@@ -8,18 +8,12 @@ namespace SensorSim.Models
 {
     public class Sensor
     {
+        public int Id { get; set; }
         public string Name {  get; set; }
         public string Location { get; set; }
         public double MinTemp { get; set; }
         public double MaxTemp { get; set; }
 
 
-        public Sensor(string name, string location, double minTemp, double maxTemp  )
-        {
-            Name = name;
-            Location = location;
-            MinTemp = minTemp;
-            MaxTemp = maxTemp;
-        }
     }
 }

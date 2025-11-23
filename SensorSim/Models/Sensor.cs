72e7e25d7547b97fe8a803b987ab88fec1b11d9c
charlize <charl@CHARPUTER>
2025-11-24 01:56:37 +1300

    Started methods ffor sensor

--- SensorSim/Models/Sensor.cs
+++ SensorSim/Models/Sensor.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
 
 namespace SensorSim.Models
@@ -15,5 +16,41 @@ namespace SensorSim.Models
         public double MaxTemp { get; set; }
 
 
+        private static readonly string filePath = Path.Combine("Data", "SensorConfig.json");
+
+        // Load sensor from config file
+        public static Sensor? LoadSensor()
+        {
+            try
+            {
+                if (!File.Exists(filePath))
+                {
+                    return null;
+                }
+
+                string jsonString = File.ReadAllText(filePath);
+
+                return JsonSerializer.Deserialize<Sensor>(jsonString) ;
+            }
+            catch
+            {
+                return null;
+            }
+        }
+
+
+        public static Sensor ValidateSensor()
+        {
+            try
+            {
+                
+                 
+            }
+            catch
+            {
+
+            }
+        }
+        
     }
 }
